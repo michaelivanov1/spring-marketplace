33e3379fec4b1a6377e861fe3cd518360731e6b5
Luke Bas <lukeallenbas@gmail.com>
2023-02-28 19:30:08 -0800

    added basic spring boot server

---
+++ backend/marketapp/src/main/java/com/marketapp/marketapp/MarketappApplication.java
@@ -0,0 +1,13 @@
+package com.marketapp.marketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketappApplication.class, args);
+	}
+
+}
