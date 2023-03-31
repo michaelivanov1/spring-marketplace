70a6e3e8b49dcc737f5d2258532b3442c8945362
Luke Bas <lukeallenbas@gmail.com>
2023-03-30 18:18:04 -0700

    added appropriate dependencies for mongo and a test http get

--- backend/marketapp/src/main/java/com/marketapp/marketapp/MarketappApplication.java
+++ backend/marketapp/src/main/java/com/marketapp/marketapp/MarketappApplication.java
@@ -8,8 +8,6 @@ package com.marketapp.marketapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MarketappApplication {
--- backend/marketapp/src/main/java/com/marketapp/marketapp/TestController.java
+++ backend/marketapp/src/main/java/com/marketapp/marketapp/TestController.java
@@ -6,14 +6,29 @@
 
 package com.marketapp.marketapp;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class TestController {
 
     @RequestMapping("/test")
     public String test() {
+
+        /*MongoClient client = MongoClients.create("mongodb+srv://admin:KqZVIcWcbn5jHe4Q@cluster0.rsb1ps8.mongodb.net/?retryWrites=true&w=majority");
+        MongoDatabase db = client.getDatabase("marketdb");
+        MongoCollection<Document> col = db.getCollection("testCollection"); //bson document
+
+        Document testDoc = new Document("_id", "1").append("name", "John the Ripper");
+        col.insertOne(testDoc);*/
+
         return "Hello World!";
+
     }
 }
