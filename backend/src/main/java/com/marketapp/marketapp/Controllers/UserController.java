faf847c3cea7c7415099165fc31849c9a3e11ee0
Luke Bas <lukeallenbas@gmail.com>
2023-06-21 16:26:31 -0700

    Added user crud methods + an update method

---
+++ backend/src/main/java/com/marketapp/marketapp/Controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.marketapp.marketapp.Controllers;
+
+import com.marketapp.marketapp.DAL.UserService;
+import com.marketapp.marketapp.ViewModels.Farmer;
+import com.marketapp.marketapp.ViewModels.User;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8080")
+@RequiredArgsConstructor
+public class UserController {
+
+
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.allUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> getSingleUserById(@PathVariable String id) {
+        return new ResponseEntity<Optional<User>>(userService.singleUserById(new ObjectId(id)), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/email/{email}")
+    public ResponseEntity<Optional<User>> getSingleUserByEmail(@PathVariable String email) {
+        return new ResponseEntity<Optional<User>>(userService.singleUserByEmail(email), HttpStatus.OK);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<Boolean> updateUser(@PathVariable ObjectId id, @RequestBody User updatedUser) {
+        User user = userService.updateUser(id, updatedUser);
+        return new ResponseEntity<Boolean>(Boolean.TRUE, HttpStatus.NO_CONTENT);
+    }
+}
---
+++ backend/src/main/java/com/marketapp/marketapp/DAL/UserService.java
@@ -0,0 +1,45 @@
+package com.marketapp.marketapp.DAL;
+
+import com.marketapp.marketapp.ViewModels.Role;
+import com.marketapp.marketapp.ViewModels.User;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> singleUserById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> singleUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User updateUser(ObjectId id, User updatedUser) {
+        User existingUser = userRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("User not found with ID: " + id));
+
+        existingUser.setProfileName(updatedUser.getProfileName());
+        existingUser.setDescription(updatedUser.getDescription());
+        existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
+        existingUser.setProfileImage(updatedUser.getProfileImage());
+        existingUser.setBannerImage(updatedUser.getBannerImage());
+        existingUser.setCreationDate(existingUser.getCreationDate());
+        existingUser.setRole(Role.USER);
+
+        return userRepository.save(existingUser);
+    }
+
+}
