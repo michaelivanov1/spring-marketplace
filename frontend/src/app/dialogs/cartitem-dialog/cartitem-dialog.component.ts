b2f63c8050b9f6b6ca6e5f8a7ecddde524468703
DMahyuddin <mahyuddin.22dylan@gmail.com>
2023-07-23 20:31:50 -0400

    bug fix for the qty going of max

--- frontend/src/app/dialogs/cartitem-dialog/cartitem-dialog.component.ts
+++ frontend/src/app/dialogs/cartitem-dialog/cartitem-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Inject, LOCALE_ID, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  LOCALE_ID,
+  Output,
+} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { formatCurrency } from '@angular/common';
 import { Produce } from '@app/common-interfaces/produce';
@@ -14,12 +20,17 @@ export class CartitemDialogComponent {
   user: any;
   produce: any;
   counter: number;
+  tempCounter: number;
   price: number;
   subTotal: string;
   cartItemForm: FormGroup;
   qoh: number;
+  isRemoved: boolean;
+  isEntered: boolean;
+  isGreater: boolean;
 
-  @Output() confirmEvent: EventEmitter<{ produce: Produce; counter: number }> = new EventEmitter<{ produce: Produce; counter: number }>();
+  @Output() confirmEvent: EventEmitter<{ produce: Produce; counter: number }> =
+    new EventEmitter<{ produce: Produce; counter: number }>();
 
   constructor(
     public dialogRef: MatDialogRef<CartitemDialogComponent>,
@@ -27,25 +38,28 @@ export class CartitemDialogComponent {
     @Inject(LOCALE_ID) public locale: string,
     private formBuilder: FormBuilder
   ) {
-
     this.cartItemForm = this.formBuilder.group({
       user: data?.user,
       produce: data?.produce,
       counter: data?.counter,
       price: data?.price,
-      subTotal: data?.subTotal
-    })
+      subTotal: data?.subTotal,
+    });
 
     this.user = data.user;
     this.produce = data.produce;
-    this.counter = 1;
+    this.counter = 0;
+    this.tempCounter = 0;
     this.price = this.produce.price;
     this.subTotal = formatCurrency(this.price, this.locale, '$ ');
     this.qoh = data.qoh;
+    this.isRemoved = false;
+    this.isEntered = false;
+    this.isGreater = false;
   }
 
   confirm(produce: Produce): void {
-    const item: { produce: Produce; user: UserStand, counter: number } = {
+    const item: { produce: Produce; user: UserStand; counter: number } = {
       produce: produce,
       user: this.user,
       counter: this.counter,
@@ -65,13 +79,12 @@ export class CartitemDialogComponent {
     this.dialogRef.close();
   }
 
-
   cancel(): void {
     console.log('canceled');
     this.dialogRef.close();
   }
 
-  qohChange(change: String): void {
+  qohChangeButton(change: String): void {
     if (change === '+' && this.counter < this.produce.qoh) {
       this.counter++;
     } else if (change === '-' && this.counter > 0) {
@@ -88,23 +101,42 @@ export class CartitemDialogComponent {
     const charCode = event.which ? event.which : event.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
       return false;
+    } else if (this.isGreater === true) {
+      return false;
     }
     return true;
   }
 
   removeNumber() {
-    if (this.counter.toString().length - 1 <= 0) {
-      this.counter = 0;
+    this.isRemoved = true;
+    if (this.tempCounter.toString().length - 1 <= 0) {
+      this.tempCounter = 0;
     } else {
-      this.counter = parseInt(
-        this.counter.toString().substring(0, this.counter.toString().length - 1)
+      this.tempCounter = parseInt(
+        this.tempCounter
+          .toString()
+          .substring(0, this.tempCounter.toString().length - 1)
       );
     }
   }
-  
-
   validNumber(event: any) {
-    this.counter = parseInt(this.counter.toString().concat(event.key));
+    this.isEntered = true;
+    this.tempCounter = parseInt(this.tempCounter.toString().concat(event.key));
+    if (this.tempCounter >= this.produce.qoh) {
+      this.isGreater = true;
+      this.tempCounter = this.produce.qoh;
+    } else {
+      this.isGreater = false;
+    }
+  }
+  updateCounterInput() {
+    if (this.isRemoved === true) {
+      this.counter = this.tempCounter;
+      this.isRemoved = false;
+    } else if (this.isEntered === true) {
+      this.counter = this.tempCounter;
+      this.isEntered = false;
+    }
     this.subTotal = formatCurrency(
       this.price * this.counter,
       this.locale,
