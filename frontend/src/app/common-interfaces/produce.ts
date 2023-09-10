2b27be194017ea2f5555af7dd844519f7ff1f0ed
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2023-09-09 23:38:16 -0400

    added produceImage attribute

--- frontend/src/app/common-interfaces/produce.ts
+++ frontend/src/app/common-interfaces/produce.ts
@@ -4,6 +4,7 @@ import { ObjectId } from '@app/common-interfaces/id-object';
 export interface Produce {
   id: ObjectId;
   foodName: string;
+  produceImage: string;
   qoh: number;
   // qoo: Number;
   harvestDate: String;
