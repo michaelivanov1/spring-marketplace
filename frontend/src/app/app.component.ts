61a3a5da1602047613cbce7a09855ead7c794ee4
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2023-09-17 18:57:02 -0400

    responsive navbar added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './authService';
 
@@ -17,6 +17,20 @@ export class AppComponent {
         window.location.pathname.substring(1, window.location.pathname.length)
       )
       : null;
+  }
+
+  // for responsive navbar. disable right side navbar div and enable extra center navbar div.
+  // this is so they can stack on top of each other on smaller screens.
+  isSmallScreen = false;
+
+  ngOnInit() {
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    // Check the window width and set isSmallScreen accordingly
+    this.isSmallScreen = window.innerWidth <= 768;
   }
 
   setTitle(title: string) {
