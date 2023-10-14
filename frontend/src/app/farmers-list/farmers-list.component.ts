54439d5d9153ccc9dbd9d790a3f29d986b86aadf
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2023-10-14 19:09:14 -0400

    added skeleton to farmers list

--- frontend/src/app/farmers-list/farmers-list.component.ts
+++ frontend/src/app/farmers-list/farmers-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile/profile.service';
 import { Profile } from '../profile/profile';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { BASEURL } from '@app/constants';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-farmers-list',
@@ -10,12 +12,47 @@ import { Observable } from 'rxjs';
 })
 export class FarmersListComponent implements OnInit {
 
-  profile$: Observable<Profile[]> | undefined;
+  profile: Observable<Profile[]> | undefined;
+  profiles: Profile[] = [];
 
-  constructor(private profileService: ProfileService) { }
+  profileImages: any = [];
+
+  constructor(
+    private profileService: ProfileService,
+    private http: HttpClient
+  ) { }
+
+
 
   ngOnInit(): void {
-    this.profile$ = this.profileService.get();
+
+    this.profile = this.profileService.get();
+
+
+    // this.profile.subscribe((data: Profile[]) => {
+    //   this.profiles = data;
+
+    //   this.profileImages = [];
+
+    //   data.forEach((p) => {
+    //     if (p.profileImage) {
+    //       this.profileImages.push(p.profileImage);
+    //     }
+    //   });
+
+
+    // const headers = new HttpHeaders().set(
+    //   'Authorization',
+    //   `Bearer ${localStorage.getItem('jwtToken')}`
+    // );
+
+    // this.http.get(`http://localhost:8080/api/file/120ddac6-afa8-44c0-a69e-6879fa2f51e0}`, { headers, responseType: 'blob' })
+    //   .subscribe((data) => {
+    //     this.profileImages = data;
+    //   });
   }
 
+  parseDate(dateString: string): Date {
+    return new Date(dateString);
+  }
 }
