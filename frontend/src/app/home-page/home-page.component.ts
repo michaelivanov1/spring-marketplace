13236890f12d258f94ecc429dee8373cf3c2ef96
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2023-11-18 20:22:44 -0500

    home page component / pics for home page component

---
+++ frontend/src/app/home-page/home-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  scrollToInfo() {
+    const infoImageContainer = document.getElementById('infoImageContainer');
+    if (infoImageContainer) {
+      infoImageContainer.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+}
