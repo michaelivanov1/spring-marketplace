95e2eb4d25c24af8bdfd5e53a29cb534324b156a
Michael Ivanov <michaelivanov153@gmail.com>
2024-03-17 23:05:40 -0400

    commented out match passwords validator

--- frontend/src/app/validators/password.validator.ts
+++ frontend/src/app/validators/password.validator.ts
@@ -5,9 +5,11 @@ export function ValidatePassword(control: AbstractControl): { invalidPassword: b
     return control.value && control.value.length < MIN_LENGTH ? { invalidPassword: true } : null;
 }
 
-export function MatchPassword(otherControl: AbstractControl) {
-  return (control: AbstractControl): { matchedPassword: boolean } | null => {
-    const isMatch = control.value === otherControl.value;
-    return isMatch ? null : { matchedPassword: false };
-  };
-}
+// function for matching double password fields 
+
+// export function MatchPassword(otherControl: AbstractControl) {
+//   return (control: AbstractControl): { matchedPassword: boolean } | null => {
+//     const isMatch = control.value === otherControl.value;
+//     return isMatch ? null : { matchedPassword: false };
+//   };
+// }
