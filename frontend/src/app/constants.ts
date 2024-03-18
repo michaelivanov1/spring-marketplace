c93a06ac97bc309b501a3c71031b512b5b7caf55
DMahyuddin <mahyuddin.22dylan@gmail.com>
2024-03-17 20:24:13 -0400

    switched to production

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,6 +1,6 @@
 // true for coding in production mode
 // false for development mode
-const isProduction = false;
+const isProduction = true;
 
 const DEVURL = 'http://localhost:8080/api/';
 const PRODURL = 'https://spring-marketplace-server.onrender.com/api/';
