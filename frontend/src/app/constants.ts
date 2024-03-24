e605807336dda14298fb481c27625f625579bd6d
Michael Ivanov <michaelivanov153@gmail.com>
2024-03-24 19:55:12 -0400

    added guesttoken const

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -6,3 +6,5 @@ const DEVURL = 'http://localhost:8080/api/';
 const PRODURL = 'https://spring-marketplace-server.onrender.com/api/';
 
 export const BASEURL = isProduction ? PRODURL : DEVURL;
+
+export const GUESTTOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtaWNoYWVsQGdtYWlsLmNvbSIsImlhdCI6MTY5NzMwNzM1MSwiZXhwIjoyNDQ0MTIyNTg2NDUyMzIwfQ.WYXFTSnGWaulKRPycbViCOHZsyzADpWSTPOwfK2xs8M';
