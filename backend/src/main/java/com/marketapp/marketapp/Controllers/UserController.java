dd5552642f37c923e786d1d30cb8801c0762c2a1
Luke Bas <lukeallenbas@gmail.com>
2024-04-06 22:52:42 -0700

    added an endpoint to get random users with given value

--- backend/src/main/java/com/marketapp/marketapp/Controllers/UserController.java
+++ backend/src/main/java/com/marketapp/marketapp/Controllers/UserController.java
@@ -1,15 +1,19 @@
 package com.marketapp.marketapp.Controllers;
 
 import com.marketapp.marketapp.DAL.UserService;
+import com.marketapp.marketapp.ViewModels.Produce;
 import com.marketapp.marketapp.ViewModels.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Collections;
+
 
 @RestController
 @RequestMapping("/api")
@@ -28,6 +32,22 @@ public class UserController {
     public ResponseEntity<Optional<User>> getSingleUserByEmail(@PathVariable String email) {
         return new ResponseEntity<Optional<User>>(userService.singleUserByEmail(email), HttpStatus.OK);
     }
+
+    @GetMapping("/user/profile/{value}")
+    public ResponseEntity <List<User>> getRandomUsers(@PathVariable String value) {
+        int randValue = Integer.parseInt(value);
+
+        //extract random elements corresponding to value passed in
+        List<User> listOfUsers = userService.allUsers();
+        List<User> listOfRandomUsers = new ArrayList<>();
+        Collections.shuffle(listOfUsers);
+        for (int i = 0; i < randValue; i++) {
+            listOfRandomUsers.add(listOfUsers.get(i));
+        }
+
+        return new ResponseEntity<>(listOfRandomUsers, HttpStatus.OK);
+    }
+    
 
     @PutMapping("/user/{email}")
     public ResponseEntity<Boolean> updateUser(@PathVariable String email, @RequestBody User updatedUser) {
